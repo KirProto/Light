a4a34027a4c4bd70cfe37fd30dc80b2ca90ff3d1
Kir Proto Q <kir.proto@ya.ru>
2021-02-09 00:25:43 +0300

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by KIR Q on 2021.02.09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
